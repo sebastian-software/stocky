23a1f174cd9cc96987d32b4b1b9b108d64a0a87d
Sebastian Werner <s.werner@sebastian-software.de>
2025-07-07 09:55:56 +0200

    feat: initial commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+const officialWallstreetSectors = [
+  "Communication Services",
+  "Consumer Discretionary",
+  "Consumer Staples",
+  "Energy",
+  "Financials",
+  "Health Care",
+  "Industrials",
+  "Information Technology",
+  "Materials",
+  "Real Estate",
+  "Utilities",
+] as const;
+
+async function main() {
+  console.log("Sectors: ");
+}
+
+void main();
