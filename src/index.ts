104f4f3caa3453dad67d346c62f76bda407156d6
Sebastian Werner <s.werner@sebastian-software.de>
2025-07-07 10:08:58 +0200

    feat: added fundamental analysis data

--- src/index.ts
+++ src/index.ts
@@ -12,8 +12,28 @@ const officialWallstreetSectors = [
   "Utilities",
 ] as const;
 
+/** Recommended value from Anne Schwedt */
+const fundamentalAnalysis = {
+  // How many shares are traded on the stock market?
+  // Very important to find out stocks which are actively traded.
+  Volume: (value: number) => value > 250000,
+
+  // How much money does the company make in percentage?
+  // Very important to find out stocks which are profitable.
+  ReturnOnEquity: (value: number) => value > 10,
+
+  // How much debt does the company have?
+  // Important to find out stocks which are not in debt.
+  DebtToEquity: (value: number) => value < 2,
+
+  // How much money does the company make in percentage?
+  // Important to find out stocks which are profitable.
+  PriceToEarnings: (value: number) => value < 20,
+};
+
 async function main() {
-  console.log("Sectors: ");
+  console.log("Sectors: ", officialWallstreetSectors);
+  console.log("Fundamental Analysis: ", fundamentalAnalysis);
 }
 
 void main();
