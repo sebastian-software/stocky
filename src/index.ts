64edea206f1dc50a0e132fbce61db9116bb735b4
Sebastian Werner <s.werner@sebastian-software.de>
2025-07-07 15:36:24 +0200

    feat: continue data processing

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { chromium, type Page } from "playwright";
+
 const officialWallstreetSectors = [
   "Communication Services",
   "Consumer Discretionary",
@@ -34,6 +36,22 @@ const fundamentalAnalysis = {
 async function main() {
   console.log("Sectors: ", officialWallstreetSectors);
   console.log("Fundamental Analysis: ", fundamentalAnalysis);
+
+  const browser = await chromium.launch({ headless: false });
+  const page = await browser.newPage();
+
+  await getStocks(page);
+
+  await page.waitForTimeout(30000);
+  await browser.close();
+}
+
+async function getStocks(page: Page) {
+  await page.goto("https://www.tradingview.com/screener/");
+  await page.keyboard.press("Shift+KeyF");
+  await page.keyboard.type("Volume");
+
+  await page.getByRole("group").getByText("Volume", { exact: true }).click();
 }
 
 void main();
